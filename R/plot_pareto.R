13dba233f2b8b101c178c2706cda40f0abb818af
celmonteil <36850573+celmonteil@users.noreply.github.com>
2019-03-29 09:23:20 +0100

    version 1.1

--- R/plot_pareto.R
+++ R/plot_pareto.R
@@ -3,20 +3,31 @@
 #' Plots graphs the population regarding each couple of objectives and emphasizes the Pareto front
 #' 
 #' @param MatObj : matrix of the objectives [NInd, nobj]
+#' @param nobj : number of objectives (optional)
+#' @param objnames : vector, length nobj, of names of the objectives (optional)
+#' @param maximized : vector of logical, length nobj, TRUE if objective need to be maximized, FALSE if minimized
 #'
 #' @examples
 #' # Definition of the population
 #' Pop <- matrix(runif(300), 100, 3)
+#' 
+#' # Definition of objectives to maximize (Obj1, Obj2) and to minimize (Obj3)
+#' maximized <- c(TRUE,TRUE,FALSE)
+#' 
 #' # Call the function
-#' plot_pareto(MatObj = Pop)
+#' plot_pareto(MatObj = Pop, maximized=maximized)
 #' 
 #' @author C. Monteil
 
-plot_pareto <- function(MatObj,nobj=NULL, objnames=NULL){
+plot_pareto <- function(MatObj,nobj=NULL, objnames=NULL,maximized=NULL){
   
   if (is.null(nobj)) nobj <- length(MatObj[1,])
   if (is.null(objnames)){objnames=paste("Obj",as.character(c(1:nobj)),sep="")}
-  is_pareto <- pareto(MatObj)
+  if (is.null(maximized)) maximized=rep(TRUE,nobj)
+  
+  MatTest=MatObj
+  MatTest[,!maximized]=-MatObj[,!maximized]
+  is_pareto <- pareto(MatTest)
   
   # Graphs
   nb_fen = choose(n = nobj, k=2)
